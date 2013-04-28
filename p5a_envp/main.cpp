2e3e723af2ad19ea46976448e135571df45ac40b
arpith <arpith@live.com>
2013-04-28 08:52:29 +0530

    added files

---
+++ p5a_envp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv, char *envp[])
+{
+        int index;
+        for(index=0; envp[index]!='\0';index++)
+        {
+                cout<<envp[index]<<endl;
+        }
+}
